5a762bef0fbfc1576f569f17d62cc973c5764130
polocto <paul.sade99@gmail.com>
2020-04-26 21:42:06 +0200

    Create biblio.h
    bibliothèque contenant l'ensemble des librairie de base pouvant servir au projet

---
+++ Projet Piscine/Projet Piscine/biblio.h
@@ -0,0 +1,16 @@
+#ifndef BIBLIO_H_INCLUDED
+#define BIBLIO_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <string>
+
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <map>
+
+#endif // BIBLIO_H_INCLUDED

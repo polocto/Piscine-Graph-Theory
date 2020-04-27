6a716f265235cea68bd21d676b3780393750f13d
polocto <paul.sade99@gmail.com>
2020-04-27 11:46:11 +0200

    Update Arete.cpp
    initialise le poids à 0

--- Projet Piscine/Projet Piscine/Arete.cpp
+++ Projet Piscine/Projet Piscine/Arete.cpp
@@ -3,7 +3,7 @@
 #include "svgfile.h"
 
 Arete::Arete(Sommet* s1, Sommet* s2)
-    :m_ext1(s1),m_ext2(s2)
+    :m_ext1(s1),m_ext2(s2),m_poids(0)
 {
     m_ext1->ajout(this);
     m_ext2->ajout(this);

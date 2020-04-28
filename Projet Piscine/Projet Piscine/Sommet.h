2878ab4b5fbee43fd7982604d752bf0b7d297e8b
polocto <paul.sade99@gmail.com>
2020-04-28 23:25:50 +0200

    Forme
    Lisibilité
    Commentaires

--- Projet Piscine/Projet Piscine/Sommet.h
+++ Projet Piscine/Projet Piscine/Sommet.h
@@ -12,44 +12,48 @@ class Sommet
 public:
     Sommet(const std::string&nom, const double& pos_x, const double& pos_y);//constructeur
     virtual ~Sommet() = default;//destructeur par default ne fait rien
+    /**Ajout d'une arete au sommet*/
+    virtual void ajout(Arete*suivant);//ajout d'une arete au sommet
+
+    /**AFFICHAGE*/
+    void affichageconsole()const;//affichage en console
+    void affichage(Svgfile& svgout)const;//affichage svg
 
     /**Getter*/
-    std::string getnom()const;
-    double getX()const;
-    double getY()const;
-    double get_vp()const;
+    std::string getnom()const;//reccupere le nom du sommet
+    double getX()const;//reccupere la position x du sommet
+    double getY()const;//reccupere la position y du sommet
+    double get_vp()const;//Reccupere l'indice de vecteur propre
+    Arete* trouverArete(Sommet* ext1);//retourne une arete
 
-    /**Ajout d'une arete au sommet*/
-    virtual void ajout(Arete*suivant);
-
-    /**Sauvegarde*/
-    void sauvegarde(std::ofstream&fichier)const;
-
-    /**Affichage*/
-    void affichageconsole()const;
-    void affichage(Svgfile& svgout)const;
-
-    /**calcule indice*/
-
+    /**CALCULE INDICES*/
+    ///indice de centralité de dergré
     void calc_icd(const size_t&ordre);
+    ///indice de vecteur propre
     void calc_vp(std::map<Sommet*,double>&somme);
     void indice_vp(std::map<Sommet*,double>&somme,const double& lambda);
+    ///indice de proximité
+    void calc_icp(double distance,double total);
+    ///intermediarite
+    //naïf
     void calc_ici_naif(double total,double a);
+    void ajoutvoisin(std::vector<Sommet*>& Som,std::map<std::string,std::pair<bool,Sommet*>>& marque,std::map<std::string,double>& poids);
+    //Brand
+    void Brand(const std::map<const Sommet*,double>&Cb,const double&n);
+    void Brand(std::map<const Sommet*,double>&distance,std::priority_queue<std::pair<const Sommet*,std::pair<const Sommet*,double>>,std::vector<std::pair<const Sommet*,std::pair<const Sommet*,double>>>,myComparator>&q,std::map<const Sommet*,double>&sigma,std::map<const Sommet*,std::list<const Sommet*>>&predecesseur)const;
+    /**Conexité*/
     int k_connexe(const Sommet* arrive)const;
     void k_connexe(int& nombre_chemin,std::map<const Arete*,bool>& arete,std::map<const Sommet*,bool>&sommet,const Sommet*arrive)const;
 
-    void calc_icp(double distance,double total);
-   void ajoutvoisin(std::vector<Sommet*>& Som,std::map<std::string,std::pair<bool,Sommet*>>& marque,std::map<std::string,double>& poids);
-   Arete* trouverArete(Sommet* ext1);
+    /**Sauvegarde*/
+    void sauvegarde(std::ofstream&fichier)const;//sauvegarde des indices
 
-   void Brand(const std::map<const Sommet*,double>&Cb,const double&n);
-   void Brand(std::map<const Sommet*,double>&distance,std::priority_queue<std::pair<const Sommet*,std::pair<const Sommet*,double>>,std::vector<std::pair<const Sommet*,std::pair<const Sommet*,double>>>,myComparator>&q,std::map<const Sommet*,double>&sigma,std::map<const Sommet*,std::list<const Sommet*>>&predecesseur)const;
 private:
     std::string m_nom;
-    std::vector<Arete*> m_suivants;
-    double m_i_d,m_i_vp,m_i_p,m_i_i,m_i_is;
-    double m_i_i_nn,m_i_d_nn;
-    double m_x,m_y;
+    std::vector<Arete*> m_suivants;//arete suivante du graphe
+    double m_i_d,m_i_vp,m_i_p,m_i_i,m_i_is;//indice normalisé
+    double m_i_i_nn,m_i_d_nn;//indice non normalisé
+    double m_x,m_y;//position du sommet
 };
 
 #endif

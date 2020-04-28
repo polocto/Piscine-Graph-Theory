0fba7bed2110019bdda23d584b00017550224032
polocto <paul.sade99@gmail.com>
2020-04-28 23:44:30 +0200

    Forme
    Mise en page
    Commentaire

--- Projet Piscine/Projet Piscine/Arete.h
+++ Projet Piscine/Projet Piscine/Arete.h
@@ -9,19 +9,26 @@ class Sommet;
 class Arete
 {
 public:
-    Arete(Sommet* s1, Sommet* s2);
-    Arete(Sommet* s1, Sommet* s2,double poids);
+    /**Construction des arete*/
+    Arete(Sommet* s1, Sommet* s2);//constructeur arete
+    Arete(Sommet* s1, Sommet* s2,double poids);//copie arete
     virtual  ~Arete() = default;
-    void affichage(Svgfile& svgout)const;
-    void ponderation(std::stringstream& ifs);
-    void affichageconsole()const;
-    double get_vp(Sommet*precedent)const;
-    double get_poid()const;
-    Sommet* getsuivant(Sommet* Som)const;
-    Sommet* getext1();
-    Sommet* getext2();
-    void k_connexe(int& nombre_chemin,std::map<const Arete*,bool>& arete,std::map<const Sommet*,bool>&sommet,const Sommet*arrive)const;
+    void ponderation(std::stringstream& ifs);//chargement des poid de l'arete
+
+    /**Affichage*/
+    void affichage(Svgfile& svgout)const;//affichage svg
+    void affichageconsole()const;//affichage console
+    /**Getter*/
+    double get_vp(Sommet*precedent)const;//reccupere l'indice de vecteur propre du sommet à l'autre extrémité "suivant"
+    double get_poid()const;//reccupere le poids de l'arete
+    Sommet* getsuivant(Sommet* Som)const;//reccup le sommet suivant
+    Sommet* getext1();//reccup sommet ext1
+    Sommet* getext2();//recup sommet ext2
+
+    /**Indice intermediarite*/
     void Brand(const Sommet*precednent,std::map<const Sommet*,double>&distance,const double&d_a,std::priority_queue<std::pair<const Sommet*,std::pair<const Sommet*,double>>,std::vector<std::pair<const Sommet*,std::pair<const Sommet*,double>>>,myComparator>&q,std::map<const Sommet*,double>&sigma,std::map<const Sommet*,std::list<const Sommet*>>&predecesseur)const;
+    /**k-arete connexité*/
+    void k_connexe(int& nombre_chemin,std::map<const Arete*,bool>& arete,std::map<const Sommet*,bool>&sommet,const Sommet*arrive)const;
 private:
     Sommet* m_ext1,* m_ext2;//extemit� des Arete
     double m_poids;

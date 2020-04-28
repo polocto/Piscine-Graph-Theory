0c9e3368d9d0453902042296db1f45b0fc53df87
polocto <paul.sade99@gmail.com>
2020-04-29 01:21:53 +0200

    Update Graph.cpp
    Suppression affichage console servant au test

--- Projet Piscine/Projet Piscine/Graph.cpp
+++ Projet Piscine/Projet Piscine/Graph.cpp
@@ -347,7 +347,7 @@ void Graph::calc_ici_naif()
             }
         }
         ///initialisation du ici
-        std::cout<<total;
+        //std::cout<<total;
         i->calc_ici_naif(total,a);
     }
 }

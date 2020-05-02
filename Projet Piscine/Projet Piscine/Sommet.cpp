600b47b44142d803439c0ded8d59cc643f4b5e66
polocto <paul.sade99@gmail.com>
2020-05-02 17:28:42 +0200

    Indice de proximité non normalisé

--- Projet Piscine/Projet Piscine/Sommet.cpp
+++ Projet Piscine/Projet Piscine/Sommet.cpp
@@ -6,14 +6,14 @@
 ///constructeur
 ///initialisation de donn�es non pass� en param�tre � 0
 Sommet::Sommet(const std::string&nom, const double& pos_x, const double& pos_y)
-    :m_nom(nom),m_i_d(0),m_i_vp(1),m_i_p(0),m_i_i(0),m_i_is(0),m_i_i_nn(0),m_i_d_nn(0),m_i_i_max(0),m_x(pos_x),m_y(pos_y)
+    :m_nom(nom),m_i_d(0),m_i_vp(1),m_i_p(0),m_i_i(0),m_i_is(0),m_i_p_nn(0),m_i_i_nn(0),m_i_d_nn(0),m_i_i_max(0),m_x(pos_x),m_y(pos_y)
 {
 }
 
 
 ///Nouvelle Proposition
 Sommet::Sommet(const Sommet* copie)
-    :m_nom(copie->m_nom),m_i_d(0),m_i_vp(1),m_i_p(0),m_i_i(0),m_i_is(0),m_i_i_nn(0),m_i_d_nn(0),m_i_i_max(0),m_x(copie->m_x),m_y(copie->m_y)
+    :m_nom(copie->m_nom),m_i_d(0),m_i_vp(1),m_i_p(0),m_i_i(0),m_i_is(0),m_i_p_nn(0),m_i_i_nn(0),m_i_d_nn(0),m_i_i_max(0),m_x(copie->m_x),m_y(copie->m_y)
 {
 
 }
@@ -38,7 +38,7 @@ void Sommet::affichageconsole()const
 {
     std::cout<<std::setprecision(3)
             <<m_nom<<" icd: ("<<std::fixed<<m_i_d<<","<<m_i_d_nn<<")"
-            <<" icp: "<<std::fixed<<m_i_p
+            <<" icp: ("<<std::fixed<<m_i_p<<","<<m_i_p_nn<<") "
             <<" ivp: "<<std::fixed<<m_i_vp
             << " icis: "<<std::fixed<<m_i_is
             <<" inter: ("<<std::fixed<<m_i_i<<", "<<std::fixed<<m_i_i_nn<<")";
@@ -65,8 +65,8 @@ void Sommet::affichage_comparaison(Sommet* ancien)const
 {
 
         std::cout<<std::setprecision(3)
-            <<m_nom<<" icd: ("<<std::fixed<<ancien->m_i_d-m_i_d<<","<<ancien->m_i_d_nn-m_i_d_nn<<")"
-            <<" icp: "<<std::fixed<<ancien->m_i_p-m_i_p
+            <<m_nom<<" icd: ("<<std::fixed<<ancien->m_i_d-m_i_d<<", "<<ancien->m_i_d_nn-m_i_d_nn<<")"
+            <<" icp: ("<<std::fixed<<ancien->m_i_p-m_i_p<<", "<<ancien->m_i_p_nn-m_i_p_nn<<")"
             <<" ivp: "<<std::fixed<<ancien->m_i_vp-m_i_vp
             << " icis: "<<std::fixed<<ancien->m_i_is-m_i_is
             <<" inter: ("<<std::fixed<<ancien->m_i_i-m_i_i<<", "<<std::fixed<<ancien->m_i_i_nn-m_i_i_nn<<")"
@@ -144,6 +144,7 @@ void Sommet::indice_vp(std::map<Sommet*,double>&somme,const double& lambda)
 ///normalise l'indice
 void Sommet::calc_icp(double distance,double total)
 {
+    m_i_p_nn=1.0/distance;
     m_i_p=total/distance;
 }
 
@@ -239,7 +240,7 @@ void Sommet::sauvegarde(std::ofstream&fichier)const
     //ecriture dans le fichier
     fichier<<" indice de centrailite de degre : ("<<m_i_d_nn<<", "<<m_i_d<<"); ";
     fichier<<"indice de vecteur propre : "<<m_i_vp<<"; ";
-    fichier<<"indice de proximite : "<<m_i_p<<"; ";
+    fichier<<"indice de proximite : ("<<m_i_p_nn<<", "<<m_i_p<<"); ";
     fichier<<"indice de centralite d'intermediarite : ("<<m_i_i_nn<<", "<<m_i_d<<"); ";
 }
 
